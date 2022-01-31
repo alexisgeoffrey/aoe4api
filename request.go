ce1ef3bcd05cd1f001ff757645cd79479920b61e
alexisgeoffrey <alexisgeoffrey1@gmail.com>
2022-01-30 20:27:37 -0600

    consolidated files

--- request.go
+++ request.go
@@ -1,7 +1,12 @@
+// Package aoe4api provides functions for retrieving
+// player data from the Age of Empires 4 leaderboard API.
 package aoe4api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -61,6 +66,8 @@ type (
 		respMap map[string]string
 	}
 )
+
+const apiUrl = "https://api.ageofempires.com/api/ageiv/Leaderboard"
 
 // Query queries the AOE4 API and returns the API response as a Response struct.
 func (r *request) Query() ([]playerInfo, error) {
@@ -127,4 +134,56 @@ func (r *request) QueryAllElo() (map[string]string, error) {
 	wg.Wait()
 
 	return sm.respMap, nil
+}
+
+func query(r *request) (response, error) {
+	payloadBytes, err := json.Marshal(r.payload)
+	if err != nil {
+		return response{}, fmt.Errorf("error marshaling json payload: %w", err)
+	}
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", apiUrl, body)
+	if err != nil {
+		return response{}, fmt.Errorf("error creating POST request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if r.userAgent != "" {
+		req.Header.Set("User-Agent", r.userAgent)
+	}
+
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return response{}, fmt.Errorf("error sending POST to API: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == http.StatusNoContent {
+			return response{}, nil
+		}
+		return response{}, fmt.Errorf("error from API, received status code %d", resp.StatusCode)
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return response{}, fmt.Errorf("error reading API response: %w", err)
+	}
+
+	var respBodyJson response
+	if err := json.Unmarshal(respBody, &respBodyJson); err != nil {
+		return response{}, fmt.Errorf("error unmarshaling json API response: %w", err)
+	}
+
+	return respBodyJson, nil
+}
+
+func getEloTypes() [5]string {
+	return [...]string{
+		"1v1",
+		"2v2",
+		"3v3",
+		"4v4",
+		"custom",
+	}
 }
--- requestbuilder.go
+++ requestbuilder.go
@@ -23,6 +23,46 @@ type (
 	requestBuilder struct{ request }
 )
 
+type Region int
+
+const (
+	Europe Region = iota
+	MiddleEast
+	Asia
+	NorthAmerica
+	SouthAmerica
+	Oceania
+	Africa
+	Global
+)
+
+type Versus int
+
+const (
+	Players Versus = iota
+	AI
+)
+
+type MatchType int
+
+const (
+	Unranked MatchType = iota
+	Custom
+	EasyAI
+	MediumAI
+	HardAI
+	ExpertAI
+)
+
+type TeamSize int
+
+const (
+	OneVOne TeamSize = iota + 1
+	TwoVTwo
+	ThreeVThree
+	FourVFour
+)
+
 func NewRequestBuilder() RequestBuilder {
 	return &requestBuilder{
 		request{
